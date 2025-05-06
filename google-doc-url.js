c45b18c2687f210df7d1ad005f9be3f2b0f9b51a
ForteHealthcare <carol@fortehealthcare.com.au>
2025-05-06 11:01:21 +1000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1SuJHMD4eqx8FxsycdxWLiLXsel9aHdnGfaoRO3J9OG8/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/148wXuoaYLqtKPTtA9ReDfriC2QZx-Kwk3p_FDXVDHsk/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyD9EyGsk_lahxv7dEQfT_lO9IwGNyWXHhg';
